cc6bea460f0829af9edcbbd12de3dbc7b36be79a
Maciej Kwaśniak <kmaciek17@gmail.com>
2018-08-25 18:25:41 -0700

    Fix babel

--- Brocfile.js
+++ Brocfile.js
@@ -3,7 +3,17 @@ const merge = require('broccoli-merge-trees');
 const lint = require('broccoli-lint-eslint');
 const mv = require('broccoli-stew').mv;
 
+const babelOptions = {
+  "presets": [
+    ["env", {
+      "targets": {
+        "node": 4
+      }
+    }]
+  ]
+}
+
 module.exports = merge([
-  mv(babel(lint('tests')), 'tests'),
-  babel(lint('src')),
+  mv(babel(lint('tests'), babelOptions), 'tests'),
+  babel(lint('src'), babelOptions),
 ]);
