ecad7d36a4ad083c661c8d1ad71b44bca94b4519
Maciej Kwaśniak <kmaciek17@gmail.com>
2021-09-22 18:51:39 +0200

    Update dependencies

--- Brocfile.js
+++ Brocfile.js
@@ -1,13 +1,12 @@
 const merge = require('broccoli-merge-trees');
 const mv = require('broccoli-stew').mv;
-const babelTranspiler = require('broccoli-babel-transpiler');
-const ESLint = require('broccoli-lint-eslint');
+const babelTranspiler = require("broccoli-babel-transpiler");
 
 const babelOptions = {
-  "presets": ["@babel/preset-env"]
+  presets: ["@babel/preset-env"],
 };
 
 module.exports = merge([
-  mv(babelTranspiler(ESLint.create('tests'), babelOptions), 'tests'),
-  babelTranspiler(ESLint.create('src'), babelOptions)
+  mv(babelTranspiler("tests", babelOptions), "tests"),
+  babelTranspiler("src", babelOptions),
 ]);
